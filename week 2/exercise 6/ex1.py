a6cee1614a92880ef9efe293445989baf00c4f7b
uaciej <amacedmace@gmail.com>
2023-12-07 13:37:15 +0100

    Week 2 Exercise 2 ex1 done

--- week 2/exercise 6/ex1.py
+++ week 2/exercise 6/ex1.py
@@ -1,5 +1,23 @@
 '''
 Now that you’ve successfully written a translator for a single English word, let’s make
-things more difficult: translate a series of English words into Pig Latin. Write a function called pl_sentence that takes a string containing several words, separated by
-spaces. (To make things easier, we won’t actually ask for a real sentence. More specifically, there will be no capital letters or punctuation.)
+things more difficult: translate a series of English words into Pig Latin. Write a function called pl_sentence that takes a string containing several words, separated by
+spaces. (To make things easier, we won’t actually ask for a real sentence. More specifically, there will be no capital letters or punctuation.)
 '''
+
+def pig_latin(word):
+    if word[0] in 'aeiou':
+        return word + 'way'
+    else:
+        return word[1:] + word[0] + 'ay'
+    
+def pl_sentence(sentence):
+    words = sentence.split()
+    translated_words = []
+    for word in words:
+        translated_words.append(pig_latin(word))
+
+    return ' '.join(translated_words)
+
+if __name__ == '__main__':
+    print(pl_sentence(input("What sentence should we translate? ")))
+
