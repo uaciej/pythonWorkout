1013d8cab64d48716135cf6b375f9c4ac77087aa
uaciej <amacedmace@gmail.com>
2023-12-07 13:24:53 +0100

    Week 2 Exercise 2 done

---
+++ week 2/exercise 6/ex1.py
@@ -0,0 +1,5 @@
+'''
+Now that you’ve successfully written a translator for a single English word, let’s make
+things more difficult: translate a series of English words into Pig Latin. Write a function called pl_sentence that takes a string containing several words, separated by
+spaces. (To make things easier, we won’t actually ask for a real sentence. More specifically, there will be no capital letters or punctuation.)
+'''
